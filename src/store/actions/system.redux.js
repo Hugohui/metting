916eef0fd479ea337a72d090412c2b81a4b80d3b
惠文华 <huiwenhua@huiwenhuadeMBP.lan>
2018-11-02 00:14:43 +0800

    commit

--- src/store/actions/system.redux.js
+++ src/store/actions/system.redux.js
@@ -2,8 +2,7 @@ import wepy from 'wepy';
 const SET_SYSTEM = 'SET_SYSTEM';
 let systemInfo = wx.getSystemInfoSync();
 let pixel = 750/systemInfo.windowWidth;
-let navHeight = systemInfo.platform == 'android'? 96 : 88,
-
+let navHeight = systemInfo.platform == 'android'? 96 : 88;
 const initState = {
   systemInfo:systemInfo,
   pixel: pixel,
